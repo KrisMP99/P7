705ef7a47fb88d83f8ebf9f05fe1c5d44850b6da
Cecilie Fog <cfog19@student.aau.dk>
2022-09-20 13:31:35 +0200

    project (API): project dependencies updated

--- P7WebApp/P7WebApp.API/Program.cs
+++ P7WebApp/P7WebApp.API/Program.cs
@@ -1,6 +1,11 @@
+using P7WebApp.Application;
+using P7WebApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
