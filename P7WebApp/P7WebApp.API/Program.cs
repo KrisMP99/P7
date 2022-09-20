b704445b48117801d46142aac36615db0349ec5e
Cecilie Fog <cfog19@student.aau.dk>
2022-09-20 13:53:01 +0200

    hide (API): hide dependency injection until database has been setup

--- P7WebApp/P7WebApp.API/Program.cs
+++ P7WebApp/P7WebApp.API/Program.cs
@@ -4,8 +4,8 @@ using P7WebApp.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
+//builder.Services.AddApplicationServices();
+//builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
