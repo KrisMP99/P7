ba36e82ccef9b58a91011f39822f5ab502f42b4d
Cecilie Fog <cfog19@student.aau.dk>
2022-09-20 12:17:13 +0200

    update (infrastructure): add dbContext

---
+++ P7WebApp/P7WebApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P7WebApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
