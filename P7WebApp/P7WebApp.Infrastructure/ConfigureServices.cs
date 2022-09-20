5f7d3048ddff2b0d80e4c3480f102c3e5d798f10
Cecilie Fog <cfog19@student.aau.dk>
2022-09-20 12:23:10 +0200

    project (infrastructure): add dependency injection

---
+++ P7WebApp/P7WebApp.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7WebApp.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    }
+}
