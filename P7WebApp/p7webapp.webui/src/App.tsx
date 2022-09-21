b40853226bb28b8b392a725156250715967daf6e
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-09-21 09:24:43 +0200

    Frontend routing working

--- P7WebApp/p7webapp.webui/src/App.tsx
+++ P7WebApp/p7webapp.webui/src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Frontpage from './Components/Frontpage/Frontpage';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
-      <Router>
           <div>
               <nav>
                   <Link to="/">Home</Link>
                   <Link to="/foo">Foo</Link>
-                  <Link to="/bar">Bar</Link>
               </nav>
+              <Routes>
+                  <Route path="/" element={<Frontpage />} />
+                  <Route path="/foo" element={<SignUp />} />
+              </Routes>
           </div>
-      </Router>
   );
 }
 
