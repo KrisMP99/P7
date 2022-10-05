7194f531672688b9ca07187721c64d1dd7ef069e
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2022-10-05 16:24:37 +0200

    Added search functionality on landingpage with dummy data

--- P7WebApp/src/p7webapp.webui/src/Components/Landingpage/Landingpage.tsx
+++ P7WebApp/src/p7webapp.webui/src/Components/Landingpage/Landingpage.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Table, Form, InputGroup} from 'react-bootstrap';
+import {dummyData} from './dummyData.js';
 
 function Landingpage(): JSX.Element {
+    const [search, setSearch] = useState('')
+    console.log(search)
     return (
         <Container>
-            <p>Hello</p>
+            <h1 className='text-center mt-4'>Find Courses</h1>
+            <Form>
+                <InputGroup className='my-3'>
+                    <Form.Control onChange={(e) => setSearch(e.target.value)} placeholder='Search for course name'/>
+                </InputGroup>
+            </Form>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Course name</th>
+                        <th>Exercises</th>
+                        <th>Members</th>
+                        <th>Owner</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {dummyData.filter((item) => {
+                    return search.toLowerCase() === '' ? item : item.Course_name.toLowerCase().includes(search)
+                }).map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.Course_name}</td>
+                        <td>{item.Exercises}</td>
+                        <td>{item.Members}</td>
+                        <td>{item.Owner}</td>
+                    </tr>
+                ))}
+
+                </tbody>
+            </Table>
+
         </Container>
     );
 }
