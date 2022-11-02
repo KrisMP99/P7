55e4ebd365bf893380fcb5e9229cdac96ed78785
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2022-11-02 12:44:10 +0100

    Exercise and ExerciseLayout added

--- P7WebApp/src/P7WebApp.Domain/AggregateRoots/CourseAggregateRoot/Exercise.cs
+++ P7WebApp/src/P7WebApp.Domain/AggregateRoots/CourseAggregateRoot/Exercise.cs
@@ -1,13 +1,14 @@
 ﻿using P7WebApp.Domain.Common;
 using P7WebApp.SharedKernel;
+using System.Reflection;
 using System.Reflection.Metadata.Ecma335;
 
 namespace P7WebApp.Domain.AggregateRoots.CourseAggregateRoot
 {
-    public class Exercise : EntityBase
+    public class Exercise : EntityBase 
     {
         public Exercise() { }
-        public Exercise(string title, bool isVisible, int exerciseNumber, DateTime startDate, DateTime endDate, DateTime createdDate, DateTime lastModifiedDate)
+        public Exercise(string title, bool isVisible, int exerciseNumber, DateTime startDate, DateTime endDate, DateTime createdDate, DateTime lastModifiedDate, ExerciseLayout layout)
         {
             Title = title;
             IsVisible = isVisible;
@@ -16,6 +17,7 @@ namespace P7WebApp.Domain.AggregateRoots.CourseAggregateRoot
             EndDate = endDate;
             CreatedDate = createdDate;
             LastModifiedDate = lastModifiedDate;
+            Layout = layout;
         }
 
         public string Title { get; set; }
@@ -25,15 +27,42 @@ namespace P7WebApp.Domain.AggregateRoots.CourseAggregateRoot
         public DateTime EndDate { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
+        public ExerciseLayout Layout { get; set; }
         public ExerciseType Type { get; set; }
 
         public void UpdateInformation(string newTitle, bool visibility, int exerciseNumber, DateTime newStartDate, DateTime newEndDate)
         {
-            Title = string.IsNullOrEmpty(newTitle) ? Title : newTitle;
-            IsVisible = visibility;
-            ExerciseNumber = exerciseNumber;
-            StartDate = newStartDate;
-            EndDate = newEndDate;
+            throw new NotImplementedException();
+        }
+
+        public Module AddModule(string title, DateTime createdDate, ExerciseLayout layout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Module DeleteModule(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSolution(string title, int exerciseNumber, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSolution(string title, int exerciseNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSubmission(string title, int exerciseNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSubmission(string title, int exerciseNumber)
+        {
+            throw new NotImplementedException();
         }
     }
 }
