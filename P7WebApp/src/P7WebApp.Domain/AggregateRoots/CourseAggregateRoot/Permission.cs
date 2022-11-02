3ee44be0b68fed6aec824541bdce496dfc041de2
kpede19 <kpede19@student.aau.dk>
2022-11-02 10:35:21 +0100

    CourseRole and Permission skeleton classes

---
+++ P7WebApp/src/P7WebApp.Domain/AggregateRoots/CourseAggregateRoot/Permission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7WebApp.Domain.AggregateRoots.CourseAggregateRoot
+{
+    public class Permission
+    {
+        public Permission()
+        {
+            
+        }
+        public bool CanUpdateCourse { get; private set; }
+        public bool CanAddExerciseGroup { get; private set; }
+        public bool CanDeleteExerciseGroup { get; private set; }
+        public bool CanAddExercise { get; private set; }
+        public bool CanUpdateExercise { get; private set; }
+        public bool CanDeleteExercise { get; private set; }
+        public bool CanCreateIniviteCode { get; private set; }
+        public bool CanRevokeInviteCode { get; private set; }
+        public bool CanViewSubmission { get; private set; }
+        public bool CanDeleteSubmission { get; private set; }
+        public bool CanCreateRoles { get; private set; }
+        public bool CanRemoveAttendee { get; private set; }
+    }
+}
