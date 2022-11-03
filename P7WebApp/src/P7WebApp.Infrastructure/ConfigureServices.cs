eb720ecc44f31f4d2c7d777cf6c9acf2c43e920d
Cecilie Fog <cfog19@student.aau.dk>
2022-11-03 11:55:40 +0100

    add dbconnectionstring

--- P7WebApp/src/P7WebApp.Infrastructure/ConfigureServices.cs
+++ P7WebApp/src/P7WebApp.Infrastructure/ConfigureServices.cs
@@ -8,6 +8,7 @@ using P7WebApp.Infrastructure.Identity;
 using P7WebApp.Domain.Repositories;
 using P7WebApp.Infrastructure.Repositories;
 using P7WebApp.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace P7WebApp.Infrastructure
 {
@@ -15,6 +16,8 @@ namespace P7WebApp.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
             //services
