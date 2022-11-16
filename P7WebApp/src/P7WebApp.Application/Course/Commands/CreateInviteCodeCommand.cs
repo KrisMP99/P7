99bb9622879b3f32d0a8ce11f583769b763146bb
Christopher <christopher.colberg@gmail.com>
2022-11-16 10:44:41 +0100

    fixed typo

--- P7WebApp/src/P7WebApp.Application/Course/Commands/CreateInviteCodeCommand.cs
+++ P7WebApp/src/P7WebApp.Application/Course/Commands/CreateInviteCodeCommand.cs
@@ -4,19 +4,19 @@ namespace P7WebApp.Application.CourseCQRS.Commands
 {
     public class CreateInviteCodeCommand : IRequest<int>
     {
-        public CreateInviteCodeCommand(int courseId, bool isActive, DateTime useableFrom, DateTime useableTo)
+        public CreateInviteCodeCommand(int courseId, bool isActive, DateTime usableFrom, DateTime usableTo)
         {
             CourseId = courseId;
             IsActive = isActive;
-            UseableFrom = useableFrom;
-            UseableTo = useableTo;
+            UsableFrom = usableFrom;
+            UsableTo = usableTo;
         }
         
         public int CourseId { get; set; }
 
         public bool IsActive { get; set; }
-        public DateTime? UseableFrom { get; set; }
-        public DateTime? UseableTo { get; set; }
+        public DateTime? UsableFrom { get; set; }
+        public DateTime? UsableTo { get; set; }
 
     }
 } 
