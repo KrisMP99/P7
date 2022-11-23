d878ff14060e671c1d8f8595b3e9a15a38993d70
kpede19 <kpede19@student.aau.dk>
2022-11-23 14:46:04 +0100

    Tests for course controller [WIP]

--- P7WebApp/src/P7WebApp.Application/CourseCQRS/CommandHandlers/DeleteCourseCommandHandler.cs
+++ P7WebApp/src/P7WebApp.Application/CourseCQRS/CommandHandlers/DeleteCourseCommandHandler.cs
@@ -1,13 +1,6 @@
 ﻿using MediatR;
 using P7WebApp.Application.Common.Interfaces;
-using P7WebApp.Application.Common.Mappings;
 using P7WebApp.Application.CourseCQRS.Commands;
-using P7WebApp.Domain.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P7WebApp.Application.CourseCQRS.CommandHandlers
 {
--- P7WebApp/tests/P7WebApp.API.Tests/UnitTests/ControllerTests/CourseControllerTests.cs
+++ P7WebApp/tests/P7WebApp.API.Tests/UnitTests/ControllerTests/CourseControllerTests.cs
@@ -7,6 +7,7 @@ using P7WebApp.Application.CourseCQRS.Commands;
 using P7WebApp.Application.CourseCQRS.Queries;
 using P7WebApp.Application.Responses;
 using P7WebApp.Domain.Aggregates.ExerciseAggregate;
+using P7WebApp.Domain.Repositories;
 
 namespace P7WebApp.Infrastructure.Tests.UnitTests.ControllerTests
 {
@@ -55,6 +56,58 @@ namespace P7WebApp.Infrastructure.Tests.UnitTests.ControllerTests
             result.Should().BeOfType<BadRequestObjectResult>();
         }
 
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public async Task CreateInviteCode_ReturnsOk_GivenValidCourseId(int courseId)
+        {
+            var mockMediator = new Mock<IMediator>();
+            var courseController = new CourseController(mockMediator.Object);
+            InviteCodeResponse inviteCodeResponse = new InviteCodeResponse();
+            mockMediator.Setup(m => m.Send(It.IsAny<int>(), It.IsAny<CancellationToken>())).
+                ReturnsAsync(inviteCodeResponse);
+
+            var result = await courseController.CreateInviteCode(courseId);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        //[Theory]
+        //[InlineData(0)]
+        //[InlineData(1)]
+        //[InlineData(100)]
+        //public async Task DeleteCourse_ReturnsOk_GivenValidCourseId(int courseId)
+        //{
+        //    var mockMediator = new Mock<IMediator>();
+        //    var courseController = new CourseController(mockMediator.Object);
+        //    int rowsAffected = 1;
+        //    mockMediator.Setup(m => m.Send(It.IsAny<int>(), It.IsAny<CancellationToken>())).
+        //        ReturnsAsync(rowsAffected);
+
+        //    var result = await courseController.DeleteCourse(courseId);
+
+        //    result.Should().BeOfType<OkObjectResult>();
+        //}
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public async Task GetCourse_ReturnsOK_GivenValidCourseId(int courseId)
+        {
+            var mockRepo = new Mock<ICourseRepository>();
+            var mockMediator = new Mock<IMediator>();
+            var courseController = new CourseController(mockMediator.Object);
+            CourseResponse courseResponse = new CourseResponse();
+            mockMediator.Setup(m => m.Send(It.IsAny<int>(), It.IsAny<CancellationToken>())).
+                ReturnsAsync(courseResponse);
+
+            var result = await courseController.GetCourse(courseId);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
         [Fact]
         public async Task GetListOfCourses_ReturnsOk_AmountIsOne()
         {
@@ -64,7 +117,7 @@ namespace P7WebApp.Infrastructure.Tests.UnitTests.ControllerTests
             mockMediator.Setup(m => m.Send(It.IsAny<GetListOfCoursesQuery>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(courseResponses);
 
-            var result = await courseController.GetListOfCourses(5);
+            var result = await courseController.GetListOfCourses(1);
 
             result.Should().BeOfType<OkObjectResult>();
         }
