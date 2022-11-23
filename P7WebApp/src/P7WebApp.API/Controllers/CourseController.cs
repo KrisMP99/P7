c012ff6258c0ddb93079796436ed7f024ee1e2c2
Kristian Pedersen <55358261+KrisMP99@users.noreply.github.com>
2022-11-23 08:56:23 +0100

    Update P7WebApp/src/P7WebApp.API/Controllers/CourseController.cs

--- P7WebApp/src/P7WebApp.API/Controllers/CourseController.cs
+++ P7WebApp/src/P7WebApp.API/Controllers/CourseController.cs
@@ -11,7 +11,6 @@ namespace P7WebApp.API.Controllers
 {
     [Route("api/courses")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    //[ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
     public class CourseController : BaseController
     {
         private readonly IMediator _mediator;
