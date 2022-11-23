9e4bf2a40fb6864523b7ef9a84c89e558bc457e3
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-11-23 08:31:14 +0100

    Update P7WebApp/src/p7webapp.webui/src/Components/Course/ExerciseGroupsOverview/ExerciseGroupsOverview.tsx
    Co-authored-by: christopher-jensen <56538222+christopher-jensen@users.noreply.github.com>

--- P7WebApp/src/p7webapp.webui/src/Components/Course/ExerciseGroupsOverview/ExerciseGroupsOverview.tsx
+++ P7WebApp/src/p7webapp.webui/src/Components/Course/ExerciseGroupsOverview/ExerciseGroupsOverview.tsx
@@ -111,7 +111,6 @@ function makeExerciseGroupElements (exercisegroups: ExerciseGroup[],
                         }
                     </div>
                 )
-
             })
 
             return (
