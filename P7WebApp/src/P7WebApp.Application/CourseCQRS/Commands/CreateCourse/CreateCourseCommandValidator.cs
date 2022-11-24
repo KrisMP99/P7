759dc1145a2fd37e99378201a653f915ef74029e
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2022-11-24 10:32:35 +0100

    Renamed files, and fixed the message.

---
+++ P7WebApp/src/P7WebApp.Application/CourseCQRS/Commands/CreateCourse/CreateCourseCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿
+using FluentValidation;
+
+namespace P7WebApp.Application.CourseCQRS.Commands.CreateCourse
+{
+    public class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
+    {
+        public CreateCourseCommandValidator()
+        {
+            RuleFor(ccc => ccc.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .NotNull().WithMessage("Title cannot be null.")
+                .MaximumLength(200).WithMessage("Title cannot exceed 200 characters.");
+
+            RuleFor(ccc => ccc.Description)
+                .NotNull().WithMessage("Description cannot be null.");
+        }
+    }
+}
---
+++ P7WebApp/src/P7WebApp.Application/CourseCQRS/Commands/DeleteExerciseGroup/DeleteExerciseGroupCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace P7WebApp.Application.CourseCQRS.Commands.DeleteExerciseGroup
+{
+    public class DeleteExerciseGroupCommandValidator : AbstractValidator<DeleteExerciseGroupCommand>
+    {
+        public DeleteExerciseGroupCommandValidator()
+        {
+            RuleFor(deg => deg.ExerciseGroupId)
+                .NotEmpty().WithMessage("Exercise group id must be provided.")
+                .NotNull().WithMessage("Exercise group id cannot be null.")
+                .GreaterThan(0).WithMessage("Exercise group id cannot be negative.");
+
+            RuleFor(deg => deg.CourseId)
+                .NotEmpty().WithMessage("Course id must be provided.")
+                .NotNull().WithMessage("Course id cannot be null.")
+                .GreaterThan(0).WithMessage("Course id cannot be negative.");
+        }
+    }
+}
