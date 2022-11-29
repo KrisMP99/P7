631da799498d7338b2127912bb90064d02c96f9d
Cecilie Fog <cfog19@student.aau.dk>
2022-11-29 11:22:02 +0100

    fix validators for profile

--- P7WebApp/src/P7WebApp.Application/ProfileCQRS/Commands/SignIn/AuthenticateCommandValidator.cs
+++ P7WebApp/src/P7WebApp.Application/ProfileCQRS/Commands/SignIn/AuthenticateCommandValidator.cs
@@ -6,8 +6,13 @@ namespace P7WebApp.Application.ProfileCQRS.Commands.SignIn
     {
         public AuthenticateCommandValidator()
         {
-            RuleFor(u => u.Username).NotEmpty().WithMessage("Username cannot be empty");
-            RuleFor(u => u.Password).NotEmpty().WithMessage("Password cannot be empty");
+            RuleFor(u => u.Username)
+                .NotEmpty().WithMessage("Username cannot be empty.")
+                .NotNull().WithMessage("Username must be valid.");
+
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage("Password cannot be empty.")
+                .NotNull().WithMessage("Password must be valid.");
         }
     }
 }
--- P7WebApp/src/P7WebApp.Application/ProfileCQRS/Commands/UpdateProfile/UpdateProfileCommandValidator.cs
+++ P7WebApp/src/P7WebApp.Application/ProfileCQRS/Commands/UpdateProfile/UpdateProfileCommandValidator.cs
@@ -6,10 +6,18 @@ namespace P7WebApp.Application.ProfileCQRS.Commands.UpdateProfile
     {
         public UpdateProfileCommandValidator()
         {
-            RuleFor(uap => uap.Email).NotEmpty().EmailAddress().WithMessage("Must be an email");
-            RuleFor(uap => uap.FirstName).NotEmpty().WithMessage("Firstname must be set");
-            RuleFor(uap => uap.LastName).NotEmpty().WithMessage("LastName must be set");
-            RuleFor(uap => uap.Email).NotEmpty().WithMessage("Password must be set");
+            RuleFor(uap => uap.Email)
+                .NotEmpty().WithMessage("Email cannot be empty.")
+                .NotNull().WithMessage("Email must be defined.")
+                .EmailAddress().WithMessage("Must be an email.");
+
+            RuleFor(uap => uap.FirstName)
+                .NotEmpty().WithMessage("First name cannot be empty.")
+                .NotNull().WithMessage("First name must be valid.");
+
+            RuleFor(uap => uap.LastName)
+                .NotEmpty().WithMessage("Last name cannot be empty.")
+                .NotNull().WithMessage("Last name must be valid.");
         }
     }
 }
