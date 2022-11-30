e6894623e5faaf8a07905d6029ab26e5ca435b25
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-11-30 13:31:25 +0100

    Create exercise now works

--- P7WebApp/src/P7WebApp.Application/ExerciseGroupCQRS/Commands/CreateExercise/CreateExerciseCommandValidator.cs
+++ P7WebApp/src/P7WebApp.Application/ExerciseGroupCQRS/Commands/CreateExercise/CreateExerciseCommandValidator.cs
@@ -12,14 +12,11 @@ namespace P7WebApp.Application.ExerciseGroupCQRS.Commands.CreateExercise
 
             RuleFor(cec => cec.IsVisible)
                 .NotNull().WithMessage("Must be specified if the exercise is visible.");
+
             RuleFor(cec => cec.Title)
                 .NotNull().WithMessage("The title cannot be null.")
                 .NotEmpty().WithMessage("Title cannot be empty.")
                 .MaximumLength(200).WithMessage("Title cannot be greater than 200 characters.");
-
-            RuleFor(cec => cec.ExerciseNumber)
-                .NotNull().WithMessage("Exercise number cannot be null.")
-                .GreaterThan(0).WithMessage("It must be specified what exercise number the exercise has.");
         }
     }
 }
