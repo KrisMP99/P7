f496c2663247ba56c5aed280dd595da60948c3a9
Cecilie Fog <cfog19@student.aau.dk>
2022-12-14 12:42:55 +0100

    wip

---
+++ P7WebApp/tests/P7WebApp.API.Tests/IntegrationTests/EndpointTests/CourseEndpointTests.cs
@@ -0,0 +1,32 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc.Testing;
+using P7WebApp.Application.Responses;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace P7WebApp.API.Tests.IntegrationTests.EndpointTests
+{
+    public class CourseEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public CourseEndpointTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void GetCourseEndpoint()
+        {
+            var client = _factory.CreateClient().RequestAuthorizationCodeTokenAsync();
+
+            var response = await client.GetAsync("api/courses/public");
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            //var courses = await client.GetFromJsonAsync<IEnumerable<CourseResponse>>(client);
+
+            //Assert.NotNull(courses);
+        }
+    }
+}
