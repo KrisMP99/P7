ed2452c99e036ca72407b6728da213d5353bf06d
Christopher <christopher.colberg@gmail.com>
2022-12-14 13:06:47 +0100

    dependencies

--- P7WebApp/tests/P7WebApp.Application.Tests/IntegrationTests/CourseTests.cs
+++ P7WebApp/tests/P7WebApp.Application.Tests/IntegrationTests/CourseTests.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace P7WebApp.Application.Tests.IntegrationTests
 {
-    internal class CourseTests
+    public class CourseTests
     {
+
+        [Fact]
+        public void CreateCourseCommandHandler_AddsCourse_GivenOneCourse()
+        {
+
+        }
     }
 }
